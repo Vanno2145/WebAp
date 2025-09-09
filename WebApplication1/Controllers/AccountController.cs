042c1a704bd3206533990ff9ff0e4e01dfa6bade
John Doe <johndoe@example.com>
2025-09-09 16:30:17 +0300

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+public class AccountController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        return View(model);
+    }
+
+    [AcceptVerbs("GET", "POST")]
+    public IActionResult IsUsernameUnique(string username)
+    {
+        var existingUsernames = new[] { "admin", "testuser", "user123" };
+        if (existingUsernames.Contains(username.ToLower()))
+        {
+            return Json(false);
+        }
+
+        return Json(true);
+    }
+
+    public IActionResult RegistrationSuccess()
+    {
+        return View();
+    }
+}
