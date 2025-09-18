63b55aa5164aaae9b2e34973185394b663f1a793
John Doe <johndoe@example.com>
2025-09-18 18:46:53 +0300

    fgdfg

---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace MyBlogMvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var newsList = new List<News>
+            {
+                new News
+                {
+                    Title = "Первая новость",
+                    Content = "Это содержание первой новости.",
+                    PublicationDate = DateTime.Now.AddDays(-2)
+                },
+                new News
+                {
+                    Title = "Вторая новость",
+                    Content = "Это содержание второй новости.",
+                    PublicationDate = DateTime.Now.AddDays(-1)
+                },
+                new News
+                {
+                    Title = "Третья новость",
+                    Content = "Это содержание третьей новости.",
+                    PublicationDate = DateTime.Now
+                }
+            };
+
+            return View(newsList);
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetTheme(string theme)
+        {
+            var options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddYears(1),
+                IsEssential = true
+            };
+
+            Response.Cookies.Append("theme", theme, options);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication1/Models/News.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
